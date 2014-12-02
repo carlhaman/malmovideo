8a777f6c7078e82bc0e1295934e1e3ad6d3d875d
Carl Håman <carl.haman@gmail.com>
2014-12-02 10:17:44 +0100

    delning av ärenden i kf
    start=
    share= skickas med till iframen

--- malmo/default.aspx.cs
+++ malmo/default.aspx.cs
@@ -24,6 +24,8 @@ namespace malmo
         private bool frontPage = false;
         private bool isFromKfAccount = false;
         private string _bctid = "0";
+        private string startTime = "";
+
         bool _kf = false;
 
         string _bodyCssClasses = "";
@@ -81,6 +83,12 @@ namespace malmo
                 queryId = Request.QueryString.GetValues("bctid").GetValue(0).ToString();
             }
 
+            //Starttime till cloudbox
+            if (Request.QueryString["start"] != null)
+            {
+                startTime = "start=" + Request.QueryString.GetValues("start").GetValue(0).ToString();
+            }
+
             if (queryId.Length > 6)
             {
                 getBrightcoveVideo(queryId, MReadToken);
@@ -96,6 +104,7 @@ namespace malmo
                         error.Visible = true;
                     }
                 }
+
                 frontPage = true;
                 if (_kf)
                 {
@@ -456,10 +465,17 @@ namespace malmo
                         playerKey = System.Configuration.ConfigurationManager.AppSettings["KF_BC_PLAYERKEY"].ToString();
                         playerId = System.Configuration.ConfigurationManager.AppSettings["KF_BC_PLAYERID"].ToString();
 
+                        //Lite ändringar för starttid och delningsfunktioner
+                        string url = meta.customFields["cblandingpage"] +"?";
+                        if (!string.IsNullOrEmpty(startTime)) {
+                            url += "&" + startTime;
+                        }
+
+                        url += "&share=" + Request.Url.GetLeftPart(UriPartial.Authority) + "/?bctid=" + _bctid;
 
                         playerHtml += "<div class=\"cb-container\">\n";
                         playerHtml += "<div class=\"embed-container\">\n";
-                        playerHtml += "<iframe src=\"" + meta.customFields["cblandingpage"] + "\" frameborder=\"0\" allowTransparency=\"true\" allowfullscreen></iframe>\n";
+                        playerHtml += "<iframe src=\"" + url + "\" frameborder=\"0\" allowTransparency=\"true\" allowfullscreen></iframe>\n";
                         playerHtml += "</div>\n";
                         playerHtml += "</div>\n";
                     }
